ed7ff8cc275730ce713038292e0d59612a1dd13a
brandon-kyle-bailey <brandonkylebailey@outlook.com>
2024-08-13 16:59:24 -0300

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,46 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
+
+export const metadata: Metadata = {
+  title: "The Startup Stack - A brilliant starter kit",
+  description:
+    "Kickstart your SaaS app development with the Startup Stack Next.js SaaS Starter Pack Template. Built with Next.js, Prisma, Stripe, Supabase, and Shadcn UI for fast, scalable, and secure SaaS solutions.",
+  openGraph: {
+    description:
+      "Kickstart your SaaS app development with the Startup Stack Next.js SaaS Starter Pack Template. Built with Next.js, Prisma, Stripe, Supabase, and Shadcn UI for fast, scalable, and secure SaaS solutions.",
+    images: [""],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "The Startup Stack",
+    description:
+      "Kickstart your SaaS app development with the Startup Stack Next.js SaaS Starter Pack Template. Built with Next.js, Prisma, Stripe, Supabase, and Shadcn UI for fast, scalable, and secure SaaS solutions.",
+    siteId: "",
+    creator: "@brandonkpbailey",
+    creatorId: "",
+    images: [""],
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          inter.variable,
+        )}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/not-found/page.tsx
@@ -0,0 +1,3 @@
+export default function NotFoundPage() {
+  return <p>Sorry, that resource doesn&apos;t exist.</p>;
+}
