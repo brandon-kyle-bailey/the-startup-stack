cfd1530fe14d978eeeb75714f2a2be8becb0bde4
brandon-kyle-bailey <brandonkylebailey@outlook.com>
2024-08-21 16:03:35 -0300

    stash

--- src/app/(company)/docs/[[...slug]]/page.tsx
+++ src/app/(company)/docs/[[...slug]]/page.tsx
@@ -1,9 +1,11 @@
 import { getCachedMarkdown } from "@/app/(company)/docs/[[...slug]]/actions";
 import FooterSection from "@/app/(components)/footer.component";
 import NavigationComponent from "@/app/(components)/navigation.component";
+import AsideComponent from "@/components/aside";
 import BreadcrumbComponent from "@/components/breadcrumb";
 import MarkdownComponent from "@/components/markdown";
 import PaginationComponent from "@/components/pagination";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import { DOCS_PAGE_ROUTES, DOCS_ROUTES } from "@/lib/content/docs/routes";
 import Link from "next/link";
 
@@ -20,54 +22,27 @@ export default async function page({ params: { slug = [] } }: PageProps) {
   return (
     <main className="p-8 lg:px-16 min-h-screen flex flex-col">
       <NavigationComponent links={DOCS_PAGE_ROUTES} />
-      <div className="w-full flex justify-between mt-10 mb-10">
-        <div className="hidden md:block pr-8 h-screen overflow-y-auto">
-          <ul className="text-muted-foreground flex flex-col gap-2">
-            {DOCS_ROUTES.map((ele, idx) => {
-              return (
-                <li key={idx}>
-                  <Link
-                    href={`/docs/${ele.href}`}
-                    className="hover:text-primary"
-                  >
-                    {ele.title}
-                  </Link>
-                  {ele.items.length > 0 && (
-                    <ul className="pl-4">
-                      {ele.items.map((subele, subeleidx) => {
-                        return (
-                          <li key={subeleidx}>
-                            <Link
-                              href={`/docs/${ele.href}/${subele.href}`}
-                              className="hover:text-primary"
-                            >
-                              {subele.title}
-                            </Link>
-                          </li>
-                        );
-                      })}
-                    </ul>
-                  )}
-                </li>
-              );
-            })}
-          </ul>
+      <div className="w-full flex justify-center gap-64 mt-10 mb-10">
+        <AsideComponent prefix="/docs" routes={DOCS_ROUTES} />
+        <div className="h-screen overflow-y-auto">
+          <ScrollArea>
+            <BreadcrumbComponent paths={slug} />
+            <MarkdownComponent>
+              <h1>{res.frontmatter.title}</h1>
+              <p className="text-muted-foreground">
+                {res.frontmatter.description}
+              </p>
+              <div>{res.content}</div>
+              <PaginationComponent
+                routes={DOCS_PAGE_ROUTES}
+                current={slug.join("/")}
+              />
+            </MarkdownComponent>
+          </ScrollArea>
         </div>
-        <div>
-          <BreadcrumbComponent paths={slug} />
-          <MarkdownComponent>
-            <h1>{res.frontmatter.title}</h1>
-            <p className="text-muted-foreground">
-              {res.frontmatter.description}
-            </p>
-            <div>{res.content}</div>
-            <PaginationComponent
-              routes={DOCS_PAGE_ROUTES}
-              current={slug.join("/")}
-            />
-          </MarkdownComponent>
+        <div className="hidden lg:block h-screen overflow-y-auto">
+          <ScrollArea>hello</ScrollArea>
         </div>
-        <div className="hidden lg:block">right</div>
       </div>
       <FooterSection />
     </main>
