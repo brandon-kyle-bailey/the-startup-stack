c2ec93ffa4916bc86f21fe88db6c6d7556603d37
brandon-kyle-bailey <brandonkylebailey@outlook.com>
2024-08-31 12:48:19 -0300

    feat: flush out dashboard page

---
+++ src/app/(product)/dashboard/(components)/main.component.tsx
@@ -0,0 +1,11 @@
+export default function DashboardMainComponent({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
+      {children}
+    </main>
+  );
+}
