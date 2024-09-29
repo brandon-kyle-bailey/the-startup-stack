3c9df98ce7fa4e1034a2466f463d8bf9b0249c3f
brandon-kyle-bailey <brandonkylebailey@outlook.com>
2024-09-29 19:03:36 -0300

    cleanup

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  eslint: {
+    dirs: ["app", "hooks", "lib"], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
+  },
+  images: {
+    remotePatterns: [
+      {
+        hostname: "avatars.githubusercontent.com",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
